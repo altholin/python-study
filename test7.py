37e179cc0b49a2d99261be213a961da87757f7fe
liwf75 <liwf75@chinaunicom.cn>
2019-10-14 22:33:28 +0800

    修正Reilly网页路径

--- test7.py
+++ test7.py
@@ -5,7 +5,7 @@ import webbrowser
 # print(r.url)
 #webbrowser.open(r.url)
 data = {'firstname': '莫烦', 'lastname': '周'}
-r = requests.post('http://pythonscraping.com/files/processing.php', data=data)
+r = requests.post('http://pythonscraping.com/pages/files/processing.php', data=data)
 print(r.text)
 # data={'zjh':'1562810212','mm':'urpscode','v_yzm':'mx9x'}
 # r = requests.post('http://jwurp.hhuc.edu.cn/loginAction.do', data=data)
